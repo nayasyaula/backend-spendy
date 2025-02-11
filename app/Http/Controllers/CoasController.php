77dbe7bee397cc4f5abcb7f11bf484405d8fe6a1
nayasyaula <[200~nayasyaulamawarni@smkwikrama.sch.idgit config --global user.email nayasyaulamawarni@smkwikrama.sch.id>
2025-02-11 08:30:47 +0700

    first commit

---
+++ app/Http/Controllers/CoasController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Coas;
+use Illuminate\Http\Request;
+
+class CoasController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Coas $coas)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Coas $coas)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Coas $coas)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Coas $coas)
+    {
+        //
+    }
+}
