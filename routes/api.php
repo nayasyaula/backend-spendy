3b1fb465f759051da5e5b78787e6e1aef29d7910
nayasyaula <[200~nayasyaulamawarni@smkwikrama.sch.idgit config --global user.email nayasyaulamawarni@smkwikrama.sch.id>
2025-02-15 22:17:53 +0700

    master coa

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,10 @@
 <?php
 
+use App\Http\Controllers\CoasController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+Route::apiResource('coas', CoasController::class);
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
